a63abf252805186a35376132ffbd07f18ae15283
Marina Klejevskaja <marina.klejevskaja@gmail.com>
2023-03-15 13:04:25 +0200

    Imports optimized

--- src/main/java/com/academy/validations/Validator.java
+++ src/main/java/com/academy/validations/Validator.java
@@ -1,10 +1,5 @@
 package com.academy.validations;
 
-import com.academy.entity.CountryCode;
-import com.academy.entity.Customer;
-import com.academy.exception.CustomerNotAdultException;
-import com.academy.exception.InvalidCountryCodeException;
-import com.academy.exception.MandatoryValueMissingException;
 import org.springframework.stereotype.Component;
 
 @Component
